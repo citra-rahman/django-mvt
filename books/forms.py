ba170128dc51b77ab03fb74ef41f7cad61391815
citra-rahman <citrapuspita.r@gmail.com>
2024-08-15 22:13:29 +0700

    add crud

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,7 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'description', 'published_date']
--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
-    path('book/new/', views.book_create, name='book_create'),
-    path('book/<int:pk>/edit/', views.book_update, name='book_update'),
-    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    url(r'', views.book_list, name='book_list'),
+    url(r'new/', views.book_create, name='book_create'),
+    url(r'^book/(?P<pk>[0-9]+)$', views.book_detail, name='book_detail'),
+    url(r'^book/(?P<pk>[0-9]+)/edit$', views.book_update, name='book_update'),
+    url(r'^book/<int:pk>/delete$', views.book_delete, name='book_delete'),
 ]
