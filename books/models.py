59b8cda83610dcaaba3d724ffcbafb5637b986ed
citra-rahman <citrapuspita.r@gmail.com>
2024-08-15 18:39:28 +0700

    create book model & applied migration

--- books/models.py
+++ books/models.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    published_date = models.DateField()
 
-# Create your models here.
+    def __str__(self):
+        return self.title
